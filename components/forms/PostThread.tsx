a0fe5113f6add801da0e16f2880cca9768001ce6
Sun3577 <151902933+Sun3577@users.noreply.github.com>
2024-04-11 01:54:38 +0900

    Comment

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -2,7 +2,6 @@
 
 import * as z from "zod";
 import { useForm } from "react-hook-form";
-import { useOrganization } from "@clerk/nextjs";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { usePathname, useRouter } from "next/navigation";
 
