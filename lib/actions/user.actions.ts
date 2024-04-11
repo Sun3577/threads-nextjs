f98f26022e467c1e716de8a2e71fcf0957e91dba
Sun3577 <151902933+Sun3577@users.noreply.github.com>
2024-04-12 03:37:06 +0900

    Profile Page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thead.model";
 
 interface Params {
   userId: string;
@@ -53,5 +54,27 @@ export async function fetchUser(userId: string) {
     //   model: Community,
   } catch (error: any) {
     throw new Error(`Failed to fetch User: ${error.message}`);
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+    return threads;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user posts: ${error.message}`);
   }
 }
