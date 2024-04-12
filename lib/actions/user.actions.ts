8bbb009168f3e7d7a191f57caeb96eeffc0d9e03
Sun3577 <151902933+Sun3577@users.noreply.github.com>
2024-04-12 15:01:09 +0900

    Community Page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,7 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
-import Thread from "../models/thead.model";
+import Thread from "../models/thread.model";
 import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
